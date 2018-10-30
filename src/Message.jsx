ba4fc8c9abadf4d72f4661295eb9ea5530fd58d6
Terence1991 <cplterencehawes@gmail.com>
2018-10-30 19:37:37 +0000

    mount added

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,1 +1,18 @@
-import React from 'react';
+//import React from 'react';
+import React, {Component} from 'react';
+
+
+class Message extends Component {
+    render() {
+        return (
+    <div className="message">
+    <span className="message-username">{this.props.userName}</span>
+    <span className="message-content">{this.props.content}</span>
+    </div>
+
+        );
+    }
+}
+
+
+export default Message;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,1 +1,29 @@
-import React from 'react';
+//import React from 'react';
+import React, {Component} from 'react';
+import Message from "./Message.jsx"
+
+
+class MessageList extends Component {
+    render() {
+    let userMessages = this.props.messages; 
+    console.log(userMessages);
+    let arrayOfMessages =  userMessages.map((message) => {
+     return <Message userName={message.username} content={message.content} id={message.id}/>
+
+});
+
+        return (
+            <main className="messages">
+             {arrayOfMessages}
+            </main>
+
+        );
+
+    }
+    
+}
+
+
+export default MessageList;
+
+
