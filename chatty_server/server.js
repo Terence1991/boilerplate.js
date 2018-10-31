85f8dc342ac8948b2e4f90d94d9dda39f31566ce
Terence1991 <cplterencehawes@gmail.com>
2018-10-31 21:04:34 +0000

    chat can apear on two seperate pages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuidv3 = require('uuid/v3')
 
 // Set the port to 3001
 const PORT = 3001;
@@ -15,18 +16,54 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+const clients = {
+
+}
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 
-  ws.on('message', function incoming(data) {
+  ws.on('message', function (data) {
     console.log(data);
     let object = JSON.parse(data);
     console.log(`user ${object.username} said ${object.content}`)
+     wss.broadcast(object);
   });
 });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    // if (client.readyState === WebSocket.OPEN) {
+       let info = checkTypeOf(data)
+      client.send(JSON.stringify(info));
+    // }
+  });
+};
+
+
+
+ const addClient = (ws, color = "black", username = 'Anonymous') => {
+  const userId = uuidv4();
+  ws.userId = userId;
+  clients[userId] = {id: userId, username , color}
+  console.log("this is my client object ",clients); 
+}
+
+const checkTypeOf = (message) => {
+  switch(message.type) {
+    case "postMessage":
+    message.type = "incomingMessage";
+    break; 
+  } 
+  return message;
+ 
+    
+}
